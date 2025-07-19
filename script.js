79918945beb054c5b4b0d89f3331b949c4a08d38
Core-Stacks <129815691+Core-Stacks@users.noreply.github.com>
2025-07-19 19:58:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function downloadApp() {
+  const downloadLink = "https://drive.google.com/file/d/1wtmm50wj4tzMs1w5_MjWqprxa7QfWIUe/view?usp=drive_link"; // Replace with real link
+  window.open(downloadLink, "_blank");
+}
