a8018cb7cd77823f9e18d9c578fc340c4de2f373
Core-Stacks <129815691+Core-Stacks@users.noreply.github.com>
2025-07-20 22:10:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,3 @@ function downloadApp() {
   const downloadLink = "https://drive.google.com/file/d/1-ItdKWZcaVXWBKSpujCx1qwswj4sEBXr/view?usp=sharing"; // Replace with real link
   window.open(downloadLink, "_blank");
 }
-function downloadG() {
-  const downloadLink = "https://drive.google.com/file/d/1sorPNMDvPBXLIkyrSDsYr9MuY3fOi5Gk/view?usp=sharing"; // Replace with real link
-  window.open(downloadLink, "_blank");
-  
-}
